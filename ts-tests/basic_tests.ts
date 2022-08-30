1ef93d7952eca5a550bece547cf45756a86583d6
Divam <dfordivam@gmail.com>
2022-08-30 13:51:52 +0900

    speculos URI: localhost -> 0.0.0.0

--- ts-tests/basic_tests.ts
+++ ts-tests/basic_tests.ts
@@ -115,9 +115,9 @@ let sendCommandAndAccept = async function(command : any, prompts : any) {
 
 let sendCommandExpectFail = async function(command : any) {
   await setAcceptAutomationRules();
-  await Axios.delete("http://localhost:5000/events");
+  await Axios.delete("http://0.0.0.0:5000/events");
 
-  let transport = await Transport.open("http://localhost:5000/apdu");
+  let transport = await Transport.open("http://0.0.0.0:5000/apdu");
   let kda = new Kda(transport);
   try { await command(kda); } catch(e) {
     return;
@@ -130,7 +130,7 @@ describe('basic tests', async function() {
 
   before( async function() {
     while(!nacl) await new Promise(r => setTimeout(r, 100));
-    let transport = await Transport.open("http://localhost:5000/apdu");
+    let transport = await Transport.open("http://0.0.0.0:5000/apdu");
     let version_string = (await transport.send(0,0xfe,0,0,Buffer.alloc(0))).slice(0,-2).toString();
     ignoredScreens.push(version_string);
   })
@@ -184,16 +184,16 @@ describe('basic tests', async function() {
   it.skip('runs a test', async () => { 
     
     await setAcceptAutomationRules();
-    await Axios.delete("http://localhost:5000/events");
+    await Axios.delete("http://0.0.0.0:5000/events");
 
-    let transport = await Transport.open("http://localhost:5000/apdu");
+    let transport = await Transport.open("http://0.0.0.0:5000/apdu");
     let kda = new Kda(transport);
     
     let rv = await kda.getPublicKey("0/0");
    
-    await Axios.post("http://localhost:5000/automation", {version: 1, rules: []});
+    await Axios.post("http://0.0.0.0:5000/automation", {version: 1, rules: []});
 
-    expect(((await Axios.get("http://localhost:5000/events")).data["events"] as [any]).filter((a : any) => a["text"] != "W e l c o m e")).to.deep.equal([
+    expect(((await Axios.get("http://0.0.0.0:5000/events")).data["events"] as [any]).filter((a : any) => a["text"] != "W e l c o m e")).to.deep.equal([
         {
           "text": "Provide Public Key",
           "x": 16,
@@ -211,7 +211,7 @@ describe('basic tests', async function() {
         }
     ]);
     expect(rv.publicKey).to.equal("04e96341109fdba54691303553ee95b371d9745410f1090055fb7c0aa9e564445483f78cb81526e27ab7869fcd996eb8bd39add229b41f9e30bccccdc00a9d6c4c");
-    await Axios.delete("http://localhost:5000/events");
+    await Axios.delete("http://0.0.0.0:5000/events");
   });
  */
 });
@@ -222,7 +222,7 @@ function testTransaction(path: string, txn: string, prompts: any[]) {
        await sendCommandAndAccept(
          async (kda : Kda) => {
            let pubkey = (await kda.getPublicKey(path)).publicKey;
-           await Axios.delete("http://localhost:5000/events");
+           await Axios.delete("http://0.0.0.0:5000/events");
 
            let rv = await kda.signTransaction(path, Buffer.from(txn, "utf-8").toString("hex"));
            expect(rv.signature.length).to.equal(128);
@@ -977,7 +977,7 @@ function testSignHash(path: string, hash: string, prompts: any[]) {
          async (kda : Kda) => {
            let pubkey = (await kda.getPublicKey(path)).publicKey;
            await toggleHashSettings();
-           await Axios.delete("http://localhost:5000/events");
+           await Axios.delete("http://0.0.0.0:5000/events");
            let rv = await kda.signHash(path, hash);
            expect(rv.signature.length).to.equal(128);
            const rawHash = hash.length == 64 ? Buffer.from(hash, "hex") : Buffer.from(hash, "base64");
@@ -1005,18 +1005,18 @@ function testSignHashFail2(path: string, hash: string) {
         // Enable and then disable
         await toggleHashSettings();
         await toggleHashSettings();
-        await Axios.delete("http://localhost:5000/events");
+        await Axios.delete("http://0.0.0.0:5000/events");
         await kda.signHash(path, hash);
       });
   }
 }
 
 let toggleHashSettings = async function() {
-  await Axios.post("http://localhost:5000/button/right", {"action":"press-and-release"});
-  await Axios.post("http://localhost:5000/button/both", {"action":"press-and-release"});
-  await Axios.post("http://localhost:5000/button/both", {"action":"press-and-release"});
-  await Axios.post("http://localhost:5000/button/right", {"action":"press-and-release"});
-  await Axios.post("http://localhost:5000/button/both", {"action":"press-and-release"});
+  await Axios.post("http://0.0.0.0:5000/button/right", {"action":"press-and-release"});
+  await Axios.post("http://0.0.0.0:5000/button/both", {"action":"press-and-release"});
+  await Axios.post("http://0.0.0.0:5000/button/both", {"action":"press-and-release"});
+  await Axios.post("http://0.0.0.0:5000/button/right", {"action":"press-and-release"});
+  await Axios.post("http://0.0.0.0:5000/button/both", {"action":"press-and-release"});
 }
 
 describe('Hash Signing Tests', function() {
@@ -1346,7 +1346,7 @@ describe("Capability Signing tests", function() {
     await sendCommandAndAccept(
       async (kda : Kda) => {
         let pubkey = (await kda.getPublicKey(path)).publicKey;
-        await Axios.delete("http://localhost:5000/events");
+        await Axios.delete("http://0.0.0.0:5000/events");
 
         let txn = await fs.readFileSync(file);
         let rv = await kda.signTransaction(path, txn);
