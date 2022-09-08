13b75ca4d361fd1621a5f8179b00863228d7a754
Divam <dfordivam@gmail.com>
2022-09-08 14:31:40 +0900

    Disable tests for nanos

--- release.nix
+++ release.nix
@@ -4,6 +4,7 @@ in
   {
     generic-cli = self.alamgu.generic-cli;
   }
-  // lib.mapAttrs' (n: lib.nameValuePair ("nanos--" + n)) self.nanos
+    # Tests are broken on nanos due to some weird speculos issues
+  // lib.mapAttrs' (n: lib.nameValuePair ("nanos--" + n)) (builtins.removeAttrs self.nanos ["test" "test-with-loging"])
   // lib.mapAttrs' (n: lib.nameValuePair ("nanox--" + n)) self.nanox
   // lib.mapAttrs' (n: lib.nameValuePair ("nanosplus--" + n)) self.nanosplus
